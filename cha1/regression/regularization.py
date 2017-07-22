7a1693ea2f6bdd2097c4ea6c7f8c1a39041a9149
Tom Wambeke <tomwambeke@tudelft.net>
2017-07-22 12:53:10 +0200

    master - regularization error computed in function of ln(lambda)

--- cha1/regression/regularization.py
+++ cha1/regression/regularization.py
@@ -23,7 +23,8 @@ class Regularization(object):
     FRMT_TRAIN = '{0}//tranining_xte.dat'
     FRMT_TEST = '{0}//test_xte.dat'
 
-    def __init__(self, Ntraining, Ntest, M, list_lnl=None):
+    def __init__(self, Ntraining, Ntest, M, descr,
+                       list_lnl=None, display=False):
         """ Create test and training data set. 
 
         :param Ntraining - int, number of data points in training dataset.
@@ -31,18 +32,22 @@ class Regularization(object):
         :param M - int, order of the polynomial. 
         """
 
-        FOLDER = 'reg_N{0}_M{1}'.format(Ntraining, M)
+        FOLDER = 'reg_N{0}_M{1}_{2}'.format(Ntraining, M, descr)
         self.train = self.FRMT_TRAIN.format(FOLDER)
         self.test = self.FRMT_TEST.format(FOLDER)        
 
         sin = SampleSinus()
         sin.target(Ntraining, self.train)
-        sin.target(Ntest, self.test, xdistr='random')
+        sin.target(Ntest, self.test, xdistr='evenly')
         SIN_FUNC = '{0}//sinus.func'.format(FOLDER)
         sin.function( SIN_FUNC )
 
+        self.RMSEs_train = []
+        self.RMSEs_test  = []
+
         # lnl = ln( lambda )
-        list_lnl = [-np.inf, -18, 0]
+        if list_lnl is None:
+            list_lnl = [-np.inf, -18, 0]
         
         for lnl in list_lnl:
             model = '{0}//poly_L{1}.func'.format(FOLDER, lnl)
@@ -60,7 +65,31 @@ class Regularization(object):
             viz.function(model, clr_option='model')
             viz.to_file( fig_name )
 
+            self.RMSEs_train.append( cs.RMSE_train )
+            self.RMSEs_test.append( cs.RMSE_test )
 
+        if display is True:
+            file_out = '{0}//regularization.png'.format( FOLDER )
+            self._display_errors( file_out, list_lnl )
 
+    
+    def _display_errors(self, file_out, list_lnl):
+        """ Display training and test errors. """
 
+        self.FIG = plt.figure(figsize=(self.FIG_W, self.FIG_H))
+        self.AX1 = self.FIG.add_axes(self.BOX)
+        self.AX1.set_xlim([list_lnl[0]-2, list_lnl[-1]+2])
+        self.AX1.set_ylim(self.YLIM)
 
+        self.AX1.plot(list_lnl, self.RMSEs_train, 
+                      marker='o',
+                      linestyle='-',
+                      linewidth=2,
+                      color=self.CLR_TRAIN)
+        self.AX1.plot(list_lnl, self.RMSEs_test, 
+                      marker='o',
+                      linestyle='-',
+                      linewidth=2,
+                      color=self.CLR_TEST)
+
+        self.FIG.savefig(file_out)
--- cha1/regression/run.py
+++ cha1/regression/run.py
@@ -3,7 +3,7 @@ from regularization import Regularization
 
 if __name__ == '__main__':
 
-    model_selection = True
+    model_selection = False
     regularization = True
 
     if model_selection:
@@ -23,5 +23,11 @@ if __name__ == '__main__':
     if regularization: 
 
         reg = Regularization(Ntraining=10,
-                             Ntest=100,
-                             M=9)
+                             Ntest=100, 
+                             M=9, descr='A')
+
+        list_lnl = [-40, -37.5, -35, -32.5, -30, -27.5, -25, -22.5, -20]
+        reg = Regularization(Ntraining=10,
+                             Ntest=100, 
+                             M=9, descr='B', 
+                             list_lnl=list_lnl, display=True)
